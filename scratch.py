b6a70236396240b24be5d0cc2126e45d6e6a5805
shawnoh <s1oh@ucsd.edu>
2020-09-01 15:15:05 -0700

    v1.1 small format changes

--- scratch.py
+++ scratch.py
@@ -1,7 +1,7 @@
-#-----------------------------------------------
+# -----------------------------------------------
 # created by Shawn Oh
 # 9/1/2020
-#-----------------------------------------------
+# -----------------------------------------------
 
 # using https://realpython.com/python-web-scraping-practical-introduction/
 
@@ -10,15 +10,15 @@ import matplotlib.patches as mp
 from matplotlib import pyplot as plt
 from urllib.request import urlopen
 
-def splitByRating(eloList, numSplit, splitList):
 
+def splitByRating(eloList, numSplit, splitList):
     splitIndex = 0
     minStd = 99999
     finalSplit = 0
     while splitIndex < len(eloList):
         firstStd = np.std(eloList[:splitIndex])
         secondStd = np.std(eloList[splitIndex:])
-        sumStd = firstStd * firstStd + secondStd * secondStd #minimize variance by minimizing sum of squares of std
+        sumStd = firstStd * firstStd + secondStd * secondStd  # minimize variance by minimizing sum of squares of std
 
         if sumStd < minStd:
             minStd = sumStd
@@ -26,33 +26,36 @@ def splitByRating(eloList, numSplit, splitList):
         splitIndex = splitIndex + 1
 
     if numSplit > 1:
-        if finalSplit != 1: #may need to change this back to 0
+        if finalSplit != 1:  # may need to change this back to 0
             splitList.append(splitByRating(eloList[0:finalSplit], numSplit - 1, splitList))
         if finalSplit != len(eloList) - 1:
             splitList.append(finalSplit + splitByRating(eloList[finalSplit:], numSplit - 1, splitList))
 
     return finalSplit
 
+
 def createColorBar(numberTeams, splitList):
-    finalbar = []
+    finalBar = []
     colIndex = 0
     ix = 0
     for splitInd in splitList:
         while ix < splitInd:
-            finalbar.append(COLORLIST[colIndex])
+            finalBar.append(COLORLIST[colIndex])
             ix = ix + 1
         colIndex = colIndex + 1
     while ix < numberTeams:
-        finalbar.append(COLORLIST[colIndex])
+        finalBar.append(COLORLIST[colIndex])
         ix = ix + 1
-    return finalbar
+    return finalBar
+
 
 def createHandles(splitList):
     handles = []
     for teamN in range(0, len(splitList) + 1):
         handleLabel = "Tier " + str(teamN + 1) + " Team"
-        handles.append(mp.Patch(color = COLORLIST[teamN], label = handleLabel))
+        handles.append(mp.Patch(color=COLORLIST[teamN], label=handleLabel))
     return handles
+
 
 def appendRegionData(regionChoice, htmlList):
     page = urlopen(BASEURL + REGIONURLLIST[regionChoice - 1])
@@ -65,6 +68,7 @@ def appendRegionData(regionChoice, htmlList):
     wholeTeamData = html[wholeTeamDataStart:wholeTeamDataEnd].split("</tr>")
     for htmlLine in wholeTeamData:
         htmlList.append(htmlLine.split("\n"))
+
 
 def openSettings(maxTeamCount, numSubdiv):
     exitFlag = False
@@ -91,9 +95,10 @@ def openSettings(maxTeamCount, numSubdiv):
             print("ERROR: Invalid selection.\n")
     return maxTeamCount, numSubdiv
 
+
 def createPlot(keys, values):
     fig, ax = plt.subplots()
-    ax.bar(keys, values, color=colorBar)
+    ax.bar(keys, values, color = colorBar)
     plotTitle = REGIONLIST[choice - 1] + " VALORANT Team Ratings"
     if MAXTEAMS < 1000:
         plotTitle = "Top " + str(MAXTEAMS) + " " + plotTitle
@@ -104,14 +109,15 @@ def createPlot(keys, values):
     eloLow = values[len(values) - 1]
     eloHigh = values[0]
     plt.ylim(eloLow - (eloLow % 100), eloHigh - (eloHigh % 100) + 100)  # dynamic y-range rounded by 100
-    plt.xticks(rotation = 90)  # rotate team names to improve clarity
-    plt.legend(handles = handleList)  # creates Team Tier key
+    plt.xticks(rotation=90)  # rotate team names to improve clarity
+    plt.legend(handles=handleList)  # creates Team Tier key
     fig.set_size_inches(16, 12)
 
     print("Close graph window to return to selection menu.")
     plt.show(block = True)
 
-COLORLIST = ["red","orange","yellow","green","blue","purple","pink","brown", "gray"]
+
+COLORLIST = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "brown", "gray"]
 REGIONLIST = ["World", "Europe", "North America", "Latin America", "Oceania", "Asia-Pacific", "Korea"]
 REGIONURLLIST = ["placeholder", "europe", "north-america", "latin-america", "oceania", "asia-pacific", "korea"]
 REGIONSHORTHAND = ["PLACEHOLDER", "EU", "NA", "LAT", "OCE", "ASIA", "KR"]
@@ -153,15 +159,15 @@ while not exitMenu:
                 if line.find("\"rank-item-team\"") >= 0:
                     a = line.find("data-sort-value=\"") + len("data-sort-value=\"")
                     b = line[a:].find('"')
-                    name = line[a:a+b]
+                    name = line[a:a + b]
                 elif line.find("rank-item-rating") >= 0:
                     a = line.find("data-sort-value=\"") + len("data-sort-value=\"")
                     b = line[a:].find('"')
-                    rating = int(line[a:a+b])
+                    rating = int(line[a:a + b])
             teamElo[name] = rating
 
         if choice == 1:
-            sortedTEV = sorted(teamElo.items(), key=lambda x: x[1], reverse = True)
+            sortedTEV = sorted(teamElo.items(), key=lambda x: x[1], reverse=True)
             for tupleX in sortedTEV:
                 dicKeys.append(tupleX[0])
                 dicValues.append(int(tupleX[1]))
